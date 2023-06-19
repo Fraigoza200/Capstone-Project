c257dbaa36f75cb359a63aca63c3d50389f94451
Francisco Raigoza <raigoza.frankie@gmail.com>
2023-06-19 11:00:38 -0700

    Card Component

---
+++ little-lemon/src/Components/CardList.jsx
@@ -0,0 +1,18 @@
+
+function Card({cards}) {
+
+  return (
+    <div className="card">
+      {cards.map((card, index) => (
+        <div key={index} className="cardcomponent">
+          <img src={card.image} alt="card photo" />
+          <h2>{card.menu_name}</h2>
+          <h3>{card.price}</h3>
+          <p>{card.description}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Card
--- little-lemon/src/Components/MainCard.jsx
+++ little-lemon/src/Components/MainCard.jsx
@@ -1,36 +1,40 @@
 import './Main.css';
-import  greeksalad from "../assets/greek_salad.jpg"
-import bruschetta from "../assets/bruschetta.jpg"
-import lemondessert from "../assets/lemon_dessert.jpg"
+import CardList from './CardList.jsx';
+import greeksalad from '../assets/greek_salad.jpg';
+import bruschetta from '../assets/bruschetta.jpg';
+import lemondessert from '../assets/lemon_dessert.jpg';
+import { useState } from 'react';
+
 function MainCard() {
-  const Card = [
+  const [cards, setCards] = useState([
     {
       image: greeksalad,
-      menu_name: "Greek Salad",
-      price: "$12.99",
-      description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo dolorum obcaecati qui eveniet velit aliquam sunt minus error autem pariatur?",
+      menu_name: 'Greek Salad',
+      price: '$12.99',
+      description:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo dolorum obcaecati qui eveniet velit aliquam sunt minus error autem pariatur?',
     },
     {
       image: bruschetta,
-      menu_name: "Bruschetta",
-      price: "$16.99",
-      description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo dolorum obcaecati qui eveniet velit aliquam sunt minus error autem pariatur?",
+      menu_name: 'Bruschetta',
+      price: '$16.99',
+      description:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo dolorum obcaecati qui eveniet velit aliquam sunt minus error autem pariatur?',
     },
     {
       image: lemondessert,
-      menu_name: "Lemon Dessert",
-      price: "$8.50",
-      description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo dolorum obcaecati qui eveniet velit aliquam sunt minus error autem pariatur?",
-    }
-  ];
-  return (
-    <section className='main_section'>
-      <div className="card">
-        
-      </div>
+      menu_name: 'Lemon Dessert',
+      price: '$8.50',
+      description:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo dolorum obcaecati qui eveniet velit aliquam sunt minus error autem pariatur?',
+    },
+  ]);
 
-    </section>
-    );
+  return (
+    <CardList
+      cards={cards}
+    />
+  );
 }
 
 export default MainCard;
